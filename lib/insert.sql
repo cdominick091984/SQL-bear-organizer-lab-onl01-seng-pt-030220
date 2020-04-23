6bc0d5daabf3d733bfd8d25fdf40db7e61345c2b
cdominick091984 <cdominick091984@gmail.com>
2020-04-23 15:26:59 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+-- INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+('Mr. Chocolate', 15, 'male', 'brown', 'calm', 1),
+('Rowdy', 6, 'female', 'rainbow', 'lovable', 1),
+('Tabitha', 11, 'female', 'pink purple', 'lovable', 1),
+('Sergeant Brown', 16, 'male', 'green black', 'tough', 1),
+('Melissa', 39, 'female', 'white', 'sweet', 1),
+('Grinch', 100, 'male', 'green', 'sad', 1),
+('Wendy', 12, 'female', 'black gold', 'happy', 1),
+(NULL, 18, 'male', 'black', 'grumpy', 1)
